e82787196126052636bd2cfde6080c3247258fcb
fullcorder <kobaring@icloud.com>
2014-08-11 02:08:56 +0900

    Initial commit.

---
+++ SwiftXibProjectSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftXibProjectSample
+//
+//  Created by fullcorder on 2014/08/11.
+//  Copyright (c) 2014年 fullcorder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
